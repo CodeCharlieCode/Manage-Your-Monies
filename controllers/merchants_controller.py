8c710bf9414293c68f809e22ee6b6285429c8586
y <codecharliecode@gmail.com>
2021-06-07 23:42:17 +0100

    added a new controller

--- controllers/merchants_controller.py
+++ controllers/merchants_controller.py
@@ -4,3 +4,8 @@ from models.merchant import Merchant
 import repositories.merchant_repository as merchant_repository
 
 merchants_blueprint = Blueprint("merchants", __name__)
+
+@merchants_blueprint.route("/merchants")
+def merchants():
+    merchants = merchant_repository.select_all()
+    return render_template("merchants/index.html", merchants = merchants)
