a45d0bdf332d90a3268ffba959667b0073d23e68
y <codecharliecode@gmail.com>
2021-06-09 23:50:11 +0100

    Created a new route within the transaction_controller uinputed and updated the html within the transaction templates

--- controllers/transactions_controller.py
+++ controllers/transactions_controller.py
@@ -1,3 +1,4 @@
+from controllers.merchants_controller import merchants
 from flask import Blueprint, Flask, redirect, render_template, request
 
 from models.transactions import Transaction
@@ -11,3 +12,20 @@ transactions_blueprint = Blueprint("transactions",__name__)
 def transactions():
     transactions = transaction_repository.select_all()
     return render_template("transactions/index.html", transactions = transactions)
+
+@transactions_blueprint.route("/transactions/new")
+def new():
+    transactions = transaction_repository.select_all()
+    merchants = merchant_repository.select_all()
+    return render_template("transactions/new.html", transactions = transactions, merchants = merchants)
+
+@transactions_blueprint.route("/transactions", methods=['POST'])
+def create_transaction():
+    merchant_id = request.form['merchant_id']
+    amount = request.form['amount']
+    description = request.form['description']
+    date = request.form['date']
+    merchant = merchant_repository.select(merchant_id)
+    transaction = Transaction(merchant, merchant, description, amount, date)
+    transaction_repository.save(transaction)
+    return redirect("/transactions")
