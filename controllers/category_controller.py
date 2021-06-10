edbbe8cfdff5a1e9b1e9893987a67fe5122c6249
y <codecharliecode@gmail.com>
2021-06-10 14:12:52 +0100

    Created a new form in the categories/new.html file and created a new route in the category_controllers.py file

--- controllers/category_controller.py
+++ controllers/category_controller.py
@@ -18,3 +18,8 @@ def show(id):
     category = category_repository.select(id)
     transactions = transaction_repository.select_all() 
     return render_template("categories/show.html", category = category, transactions = transactions)
+
+@category_blueprint.route("/categories/new/")
+def new():
+    categories = category_repository.select_all
+    return render_template("categories/new.html", categories = categories)
